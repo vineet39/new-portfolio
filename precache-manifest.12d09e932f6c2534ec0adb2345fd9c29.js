ff5a5a27ff36868ca6ef072ece54fd44b49679b0
vineet39 <vinit.bugtani@gmail.com>
2020-11-28 23:57:11 +1100

    Updates

---
+++ precache-manifest.12d09e932f6c2534ec0adb2345fd9c29.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b229f9876561797bc2abe95b50e1599d",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "7502f53d1b30922f81d1",
+    "url": "/new-portfolio/static/css/2.6056e389.chunk.css"
+  },
+  {
+    "revision": "841d2288df0287f95bfc",
+    "url": "/new-portfolio/static/css/main.63ad455a.chunk.css"
+  },
+  {
+    "revision": "7502f53d1b30922f81d1",
+    "url": "/new-portfolio/static/js/2.5b832502.chunk.js"
+  },
+  {
+    "revision": "6c88e50fd546ef59bde03720d9766cbb",
+    "url": "/new-portfolio/static/js/2.5b832502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841d2288df0287f95bfc",
+    "url": "/new-portfolio/static/js/main.5933f821.chunk.js"
+  },
+  {
+    "revision": "479325996ac7fdb04c9c",
+    "url": "/new-portfolio/static/js/runtime-main.6c8eeb54.js"
+  },
+  {
+    "revision": "6907bf0f1825d2c70964b27f7f230e74",
+    "url": "/new-portfolio/static/media/bank.6907bf0f.jpg"
+  },
+  {
+    "revision": "00d967dc4ae8351e51e0a08efc454f56",
+    "url": "/new-portfolio/static/media/burger.00d967dc.png"
+  },
+  {
+    "revision": "66b21d776cedcd26b5817dce549789a4",
+    "url": "/new-portfolio/static/media/carrental_one.66b21d77.jpg"
+  },
+  {
+    "revision": "da2e8f9f9f1c0e138da820c997754824",
+    "url": "/new-portfolio/static/media/carrental_two.da2e8f9f.jpg"
+  },
+  {
+    "revision": "034163ba81a166de2910fbeda07775f9",
+    "url": "/new-portfolio/static/media/dating.034163ba.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/new-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/new-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/new-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/new-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/new-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b244c8097fb5875f28ccb99bedfb4873",
+    "url": "/new-portfolio/static/media/jemena.b244c809.png"
+  },
+  {
+    "revision": "f0377e65995cc2c6039566b91d8733e5",
+    "url": "/new-portfolio/static/media/psd.f0377e65.png"
+  },
+  {
+    "revision": "bec60248a46241aa4dc86b83ec380537",
+    "url": "/new-portfolio/static/media/rpc.bec60248.jpg"
+  }
+]);
