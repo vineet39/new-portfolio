68b420813a3056cc2c43e1e56bce219eeafe5e98
vineet39 <vinit.bugtani@gmail.com>
2021-02-20 21:13:28 +1100

    Site metadata added

--- src/App.js
+++ src/App.js
@@ -19,18 +19,17 @@ export default class App extends Component {
       <Aux>
         <Helmet>
           <html lang="en" />
-          <meta name="keywords" content="Vineet, Bugtani, RMIT, Portfolio, Software, Developer, Profile, Personal, Website, Github, Graduate, Masters, IT, Information, Technology, Application, App, Engineer" />
+          <meta name="keywords" content="Vineet, Bugtani, Software, Developer, Portfolio, Graduate, RMIT, Profile, Personal, Website, Github, Masters, IT, Information, Technology, Application, App, Engineer" />
           <meta name="description" content="Vineet Bugtani's personal portfolio. Graduate software developer actively seeking a job in the software industry." />
           <meta name="author" content="Vineet Bugtani" />
           <meta name="google-site-verification" content="YHzQSrlf8B4xrNccsH6r0Qx3iP5SJ4H6AdwIYIKwpUw" />
-          <title>Vineet Bugtani</title>
+          <title>Vineet Bugtani - Software Developer Portfolio</title>
         </Helmet>
         <ScrollToTop />
         <Profile />
         <WorkExperience />
-         <Projects />
-      <Contact />
-
+        <Projects />
+        <Contact />
       </Aux>
     );
   }
